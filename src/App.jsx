9753ba57d2919ddef5b55404eb320d0d1d9aeb32
AnkitKumar666 <ankitk251002@gmail.com>
2025-03-31 16:15:25 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UploadForm from './components/UploadForm';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>File Upload System</h1>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
